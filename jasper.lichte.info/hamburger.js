4c6a076dd510227f4053d52d296e670521f3fdbb
jali9254 <34079475+jali9254@users.noreply.github.com>
2018-06-08 17:21:47 +0200

    .

--- jasper.lichte.info/hamburger.js
+++ jasper.lichte.info/hamburger.js
@@ -3,11 +3,28 @@ const hamburger_nodes = {
     close : document.querySelector('nav #close'),
     open : document.getElementById('hamburger_open')
 };
+if (window.innerWidth <= 500) {
+    hamburger_nodes.nav.style.left = '-100vw';
+} else {
+    hamburger_nodes.nav.style.left = '-400px';
+}
 
 hamburger_nodes.close.addEventListener('click', e => {
-    hamburger_nodes.nav.style.display = 'none';
+    if (window.innerWidth <= 500) {
+        hamburger_nodes.nav.style.left = '-100vw';
+    } else {
+        hamburger_nodes.nav.style.left = '-400px';
+    }
 });
 
 hamburger_nodes.open.addEventListener('click', e => {
-    hamburger_nodes.nav.style.display = 'block';
+    hamburger_nodes.nav.style.left = '0';
 });
+
+addEventListener('resize', e => {
+    if (window.innerWidth <= 500) {
+        hamburger_nodes.nav.style.left = '-100vw';
+    } else {
+        hamburger_nodes.nav.style.left = '-400px';
+    }
+})
