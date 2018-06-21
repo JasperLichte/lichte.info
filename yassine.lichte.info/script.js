3fda7fa682bde85a650da333f25e17513dbadc2b
jali9254 <34079475+jali9254@users.noreply.github.com>
2018-06-21 21:07:38 +0200

    .

---
+++ yassine.lichte.info/script.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+
+    let a = document.querySelectorAll('.huhu');   
+
+    for(let c = 0; c < a.length; c++) {
+        a[c].innerHTML = 'hh';
+    }
+
+});
