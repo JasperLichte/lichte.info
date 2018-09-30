ba1a9106aa99857b02a53f6067322d195667ca75
JasperLichte <contact@jasper-lichte.com>
2018-09-30 18:50:24 +0200

    .

--- jasper.lichte.info/projects/react-modules/src/components/common/text/Header2/Header2.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import './Header2.css';
-class Header2 extends Component {
-  render = () => (
-      <h2 className="Header2">{this.props.children}</h2>
-  );
-}
-
-export default Header2;
--- jasper.lichte.info/projects/todo-app/static/js/common/base.js
+++
@@ -1,7 +0,0 @@
-window._$ = function(selector) {
-    var allNodes = document.querySelectorAll(selector);
-    if (allNodes.length > 1) {
-        return allNodes;
-    }
-    return document.querySelector(selector);
-}
